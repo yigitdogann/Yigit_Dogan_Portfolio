b88b4e8289cf110e3f14c83b409691db5a3df3ce
Yiğit Doğan <yigitdogan2k@gmail.com>
2023-11-04 14:22:57 +0300

    v2

---
+++ script.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll("ul li");
+const pages = document.querySelectorAll(".page");
+const allBtns = document.querySelectorAll(".close-btn");
+
+let selectedPageBtn = "";
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    for (let i = 0; i < pages.length; i++) {
+      pages[i].style.display = "none";
+    }
+    pages[i].style.display = "block";
+    selectedPageBtn = i;
+    document.querySelector("header").style.filter = "blur(2px)";
+  });
+}
+
+for (let i = 0; i < allBtns.length; i++) {
+  allBtns[i].addEventListener("click", function () {
+    allBtns[i].parentElement.style.display = "none";
+    document.querySelector("header").style.filter = "blur(0px)";
+  });
+}
